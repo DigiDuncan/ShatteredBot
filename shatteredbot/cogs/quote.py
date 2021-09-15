5f2042fb727b5910420c7c1ca4912ffc7d6859a3
DigiDuncan <digiduncan@gmail.com>
2021-09-15 12:56:58 -0400

    start quote object

--- shatteredbot/cogs/quote.py
+++ shatteredbot/cogs/quote.py
@@ -1,8 +1,28 @@
 import logging
 
+import arrow
+
 from discord.ext import commands
+from shatteredbot.lib.utils import sentence_join
 
 logger = logging.getLogger("shatteredbot")
+
+
+class Quote:
+    def __init__(self, qid: int, text: str, authors: list[str], *,
+                 custom_author: str = None):
+        self.qid = qid
+        self.text = text
+        self._authors = authors
+        self.year = arrow.now().year
+        self._custom_author = custom_author
+
+    @property
+    def authors(self):
+        if self._custom_author:
+            return self._custom_author
+        else:
+            return sentence_join(self._authors, oxford = True)
 
 
 class QuoteCog(commands.Cog):
